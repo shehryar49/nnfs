eb2bd0f0f046f1090230d7c9105322a0563a9b1b
shehryar49 <shehryarshahid49@gmail.com>
2024-04-26 21:43:36 +0500

    Add bias

--- xor.py
+++ xor.py
@@ -3,6 +3,7 @@ import random
 # Activation functions
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+# Activation function derivatives
 def sigmoid_derivative(x):
     return x*(1-x)
 
@@ -10,19 +11,23 @@ def sigmoid_derivative(x):
 def MSE(aL,y):
     l = aL - y
     return np.sum(np.square(l)) / len(y)
+def CEL(aL,y):
+    pass
+# Error function derivatives
 def MSE_derivative(aL,y):
     return 2 * (aL - y)
 def CEL_derivative(aL,y):
     return aL - y
+
 class Layer:
     def __init__(self,num_neurons,num_inputs,fn = None,fnd = None,err_fun_deriv=MSE_derivative):
-        self.n = num_neurons
         self.weights = np.random.randn(num_inputs,num_neurons)
+        self.bias = np.random.randn(1,num_neurons).flatten()
         self.fn = fn
         self.fnd = fnd
         self.err_fnd = err_fun_deriv
     def forward(self,inputs):
-        res = np.matmul(inputs,self.weights)
+        res = np.matmul(inputs,self.weights) + self.bias
         if self.fn != None:
             res = self.fn(res)
         self.aL = res
@@ -33,11 +38,12 @@ class Layer:
             ld = self.err_fnd(self.aL,y) 
         else:
             ld = self.fnd(self.aL) * self.err_fnd(self.aL,y)
-        return ld
+        return ld    
     # deltas at hidden layers
     def deltas(self,dout,dw): # dw are weights of next layer
         a = np.matmul(dout,dw.T)
-        a = self.fnd(self.aL) * a
+        if self.fnd !=None:
+            a = self.fnd(self.aL) * a
         return a
         
     def backpropagate(self,a,deltas,lr = 1):
@@ -49,23 +55,23 @@ class Layer:
         inputs = np.repeat(inputs.T,len(deltas),axis=1)
         delta_weight = inputs*deltas
         self.weights = self.weights - lr*delta_weight
-
+        # update bias
+        self.bias = self.bias - lr*deltas
 
 # XOR 
 # 0 0 0
 # 0 1 1
 # 1 0 1
 # 1 1 0
-
-
 inputs = np.array([[0,0],[0,1],[1,0],[1,1]])
 y = np.array([[0],[1],[1],[0]])
-w1 = np.array([[1,0],[0,1]])
 
-# hidden layer 1
+# hidden layer with 2 neurons 2 inputs each
 hidden = Layer(2,2,sigmoid,sigmoid_derivative,MSE_derivative)
+# output layer with 1 neuron having 2 inputs
 output = Layer(1,2,sigmoid,sigmoid_derivative,MSE_derivative)
-for i in range(1000000):
+
+for i in range(10000):
     a1 = hidden.forward(inputs)
     a2 = output.forward(a1)
     print('Loss = ',MSE(output.aL,y))
